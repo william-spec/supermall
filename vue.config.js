066123273b022e1c897bb4a21967b5949011ae83
William <1833063260@qq.com>
2021-09-18 22:43:21 +0800

    change

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  publicPath: './',   //所有的资源都会被链接为相对路径，这样打出来的包可以被部署在任意路径，相当于静态资源的目录
+  outputDir: 'dist',  //默认就是dist
   devServer: {
     port: 8889,   //打开的端口号
     open: true    //运行后自动打开浏览器
