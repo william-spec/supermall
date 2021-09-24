0991657c3a802845f66d544a5b1e3c4c2741b717
William <1833063260@qq.com>
2021-09-25 00:44:39 +0800

    change

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.listen(8888, () => {console.log('启动成功，访问"localhost:8888"');})
 //由于该后端需要放在服务器上运行，所以共享服务器的域名或ip地址，并且ssl证书只能绑定一个域名或者ip地址，所以此处需要使用与服务器相同的SSL证书
 //当该后端运行于非使用该SSL服务器下时，会提示https无效，因为SSL证书只能绑定一个域名或ip地址，被绑定在使用该SSL的服务器上
 // let options = {
-//   key: fs.readFileSync('./sslCertification/private.key'),  //私钥文件路径
-//   cert: fs.readFileSync('./sslCertification/certificateAndca_bundle.crt')  //证书文件路径
+//   key: fs.readFileSync('./sslCertification/private.key'),  //私钥文件路径，项目中已删除，需重新添加
+//   cert: fs.readFileSync('./sslCertification/certificateAndca_bundle.crt')  //证书文件路径，项目中已删除，需重新添加
 // };
 // https.createServer(options, app.callback()).listen(8888, () => {
 //   console.log(`server running success at 8888`)
